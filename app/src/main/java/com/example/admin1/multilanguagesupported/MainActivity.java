acb800838f0f26ce02b465ea33bb5ae48beece90
dharavp <pateldhara248@gmail.com>
2017-08-23 16:53:46 +0530

    add onConfiguration changed

--- app/src/main/java/com/example/admin1/multilanguagesupported/MainActivity.java
+++ app/src/main/java/com/example/admin1/multilanguagesupported/MainActivity.java
@@ -6,11 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private  Locale myLocale;
+    private Locale myLocale;
+    private TextView welcome, email, password, login, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button hindi = (Button) findViewById(R.id.hindi);
         Button english = (Button) findViewById(R.id.english);
+        welcome = (TextView) findViewById(R.id.welcome);
+        email = (TextView) findViewById(R.id.email);
+        password = (TextView) findViewById(R.id.password);
+        login = (TextView) findViewById(R.id.login);
+        signup = (TextView) findViewById(R.id.signup);
         hindi.setOnClickListener(this);
         english.setOnClickListener(this);
     }
@@ -45,8 +54,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Configuration config = new Configuration();
         config.locale = myLocale;
         getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-        Intent refresh = new Intent(this, MainActivity.class);
-        startActivity(refresh);
-        finish();
+        onConfigurationChanged(config);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        welcome.setText(R.string.welcome);
+        email.setText(R.string.email);
+        login.setText(R.string.login);
+        password.setText(R.string.password);
+        signup.setText(R.string.signup);
     }
 }
